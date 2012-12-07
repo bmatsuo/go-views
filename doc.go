660ac62633b35144f8f634ce9e9914c949f890db
Bryan Matsuo <bryan.matsuo@gmail.com>
2012-12-06 23:17:09 -0800

    add godoc documentation

---
+++ doc.go
@@ -0,0 +1,14 @@
+/*
+compile a view hierarchy to a golang source file
+
+	go-views views/ > templates.go
+
+automatically recompile a hierarchy on file edits
+
+	go-views -w -o templates.go views/
+
+for help
+
+	go-views -h
+*/
+package documentation
